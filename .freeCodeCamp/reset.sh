6b2e985f3a7b8175643446cb52f9609e673e3157
moT01 <20648924+moT01@users.noreply.github.com>
2021-06-15 14:31:16 -0500

    80.1

--- .freeCodeCamp/reset.sh
+++ .freeCodeCamp/reset.sh
@@ -1,3 +1,4 @@
 sudo cp /workspace/project/.freeCodeCamp/reset_files/kitty_ipsum_1.txt /workspace/project
 sudo cp /workspace/project/.freeCodeCamp/reset_files/kitty_ipsum_2.txt /workspace/project
 sudo cp /workspace/project/.freeCodeCamp/reset_files/stdout.txt /workspace/project
+sudo cp /workspace/project/.freeCodeCamp/reset_files/stderr.txt /workspace/project
