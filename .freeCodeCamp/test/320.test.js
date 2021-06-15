96adf84c0a3cf2d82ec6aa860f20d6613ebec9a7
moT01 <20648924+moT01@users.noreply.github.com>
2021-06-15 14:33:06 -0500

    330.1

--- .freeCodeCamp/test/320.test.js
+++ .freeCodeCamp/test/320.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getLastCommand, getFileContents } = require('./utils');
 
 describe('tests', () => {
@@ -17,4 +17,4 @@ describe('tests', () => {
 
     assert(correctCommand && correctRedirect && /freeCodeCamp/g.test(fileContents));
   });
-});
+});*/
