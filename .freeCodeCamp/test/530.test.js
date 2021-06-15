a016a3c3d99547f72810198404a1c55cff89bb56
moT01 <20648924+moT01@users.noreply.github.com>
2021-06-15 14:34:35 -0500

    530.1

--- .freeCodeCamp/test/530.test.js
+++ .freeCodeCamp/test/530.test.js
@@ -1,12 +1,12 @@
-/*const assert = require('assert');
+const assert = require('assert');
 const { getLastCommand } = require('./utils');
 
 describe('tests', () => {
   it('should pass', async () => {
     const lastCommand = await getLastCommand();
     const correctFlags = (lastCommand.indexOf('--color') > 0 || lastCommand.indexOf('--colour') > 0) && (lastCommand.indexOf('-n') > 0 || lastCommand.indexOf('--line-number') > 0);
-    const filteredCommand = lastCommand.filter(part => part != '--color' && part != '--colour' && part !== '-n' && part !== '--line-number');
+    const filteredCommand = lastCommand.filter(part => part !== '--color' && part !== '--colour' && part !== '-n' && part !== '--line-number');
 
-    assert(correctFlags && filteredCommand[0] === 'grep' && filteredCommand[1] === 'meow' && filteredCommand[2] === 'kitty_ipsum_1.txt');
+    assert(correctFlags && filteredCommand[0] === 'grep' && filteredCommand[1].pattern === 'meow[a-z]*' && filteredCommand[2] === 'kitty_ipsum_1.txt');
   });
-});*/
+});
