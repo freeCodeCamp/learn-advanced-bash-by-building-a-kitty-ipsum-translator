3d0ddfc5ee19285622908b5cb05b84517163a064
moT01 <20648924+moT01@users.noreply.github.com>
2021-06-15 14:33:37 -0500

    400.1

---
+++ .freeCodeCamp/test/400.test.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+const { getLastCommand } = require('./utils');
+
+describe('tests', () => {
+  it('should pass', async () => {
+    const lastCommand = await getLastCommand();
+
+    assert(lastCommand[0] === 'wc' && lastCommand[1].op === '<' && lastCommand[2] === 'kitty_ipsum_1.txt');
+  });
+});
