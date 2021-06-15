21f834143e3b449bbf9de5042956f2c1873386b3
moT01 <20648924+moT01@users.noreply.github.com>
2021-06-15 14:31:11 -0500

    70.1

---
+++ .freeCodeCamp/test/70.test.js
@@ -0,0 +1,11 @@
+const assert = require('assert');
+const { getLastCommand } = require('./utils');
+
+describe('tests', () => {
+  it('should pass', async () => {
+    const lastCommand = await getLastCommand();
+    const correctCommand = lastCommand[0] === 'bad_command' && lastCommand[1].op === '>' && /stderr\.txt$/.test(lastCommand[2]);
+
+    assert(correctCommand);
+  });
+});
