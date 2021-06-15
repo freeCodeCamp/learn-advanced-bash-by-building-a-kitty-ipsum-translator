4e42317eec62f346b3ee928d76e43cd851deecf0
moT01 <20648924+moT01@users.noreply.github.com>
2021-06-15 14:33:50 -0500

    430.1

--- .freeCodeCamp/test/420.test.js
+++ .freeCodeCamp/test/420.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getLastCommand, getFileContents } = require('./utils');
 
 describe('tests', () => {
@@ -14,4 +14,4 @@ describe('tests', () => {
 
     assert(correctCommand && /~~ kitty_ipsum_1\.txt info ~~/g.test(fileContents));
   });
-});
+});*/
