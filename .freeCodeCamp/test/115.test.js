6370a0351e4735feb2056b1753b33467a722bbde
moT01 <20648924+moT01@users.noreply.github.com>
2021-06-15 14:31:32 -0500

    120.1

--- .freeCodeCamp/test/115.test.js
+++ .freeCodeCamp/test/115.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getLastCommand } = require('./utils');
 
 describe('tests', () => {
@@ -8,4 +8,4 @@ describe('tests', () => {
 
     assert(lastCommand[0] === 'echo' && lastCommand[1] === '' && /echo[ \t]+\$NAME/g.test(lastCommandAsTyped));
   });
-});
+});*/
