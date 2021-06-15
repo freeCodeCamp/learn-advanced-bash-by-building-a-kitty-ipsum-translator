26b77b1f75239d00d69fb3974e2df2487543ced1
moT01 <20648924+moT01@users.noreply.github.com>
2021-06-15 14:30:58 -0500

    40.1

---
+++ .freeCodeCamp/test/40.test.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+const { getLastCommand, getFileContents } = require('./utils');
+
+describe('You', () => {
+  it('should enter the suggested command', async () => {
+    const lastCommand = await getLastCommand();
+    const fileContents = await getFileContents('../stdout.txt');
+    const correctCommand = lastCommand[0] === 'echo' && lastCommand[1] === 'hello' && lastCommand[2] === 'bash' && lastCommand[3].op === '>' && /stdout\.txt$/.test(lastCommand[4]);
+
+    assert(correctCommand && fileContents.match(/hello bash/g).length === 1);
+  });
+});
