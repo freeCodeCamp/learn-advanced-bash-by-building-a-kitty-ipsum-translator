68d297050d025099bdebd811e0a5cb8764665630
moT01 <20648924+moT01@users.noreply.github.com>
2021-06-15 14:33:28 -0500

    380.1

---
+++ .freeCodeCamp/test/380.test.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+const { getLastCommand } = require('./utils');
+
+describe('You', () => {
+  it('should enter the suggested command', async () => {
+    const lastCommand = await getLastCommand();
+
+    assert(lastCommand[0] === 'wc' && (lastCommand[1] === 'kitty_ipsum_1.txt' || lastCommand[1] === './kitty_ipsum_1.txt'));
+  });
+});
