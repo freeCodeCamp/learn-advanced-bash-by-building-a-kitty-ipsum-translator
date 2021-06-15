9922d1bf42072893851ca3b0142729b5c240f58a
moT01 <20648924+moT01@users.noreply.github.com>
2021-06-15 14:33:24 -0500

    370.1

--- .freeCodeCamp/test/370.test.js
+++ .freeCodeCamp/test/370.test.js
@@ -1,12 +1,12 @@
-/*const assert = require('assert');
+const assert = require('assert');
 const { getLastCommand } = require('./utils');
 
 describe('tests', () => {
   it('should pass', async () => {
     const lastCommand = await getLastCommand();
-    const correctFlag = lastCommand.indexOf('-w') >= 0 || lastCommand.indexOf('--words') >= 0;
+    const correctFlag = lastCommand.indexOf('-m') >= 0 || lastCommand.indexOf('--chars') >= 0;
     const correctArg = lastCommand.indexOf('kitty_ipsum_1.txt') >= 0;
 
     assert(lastCommand[0] === 'wc' && correctFlag && correctArg);
   });
-});*/
+});
