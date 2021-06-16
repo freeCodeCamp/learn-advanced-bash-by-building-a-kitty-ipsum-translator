435928ecc805268df6e3d7564e823635b24d65c9
moT01 <20648924+moT01@users.noreply.github.com>
2021-06-16 12:15:50 -0500

    178.1

---
+++ .freeCodeCamp/test/178.test.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+const { getNextCommand } = require('./utils');
+
+describe('You', () => {
+  it('should enter the suggested command', async () => {
+    const nextCommand = await getNextCommand();
+
+    assert(nextCommand[0] === 'cat' && (nextCommand[1] === undefined || nextCommand[1].op === ';'));
+  });
+});
