db826e58ee0eedcb6841ef6ffd942187d485ec2e
moT01 <20648924+moT01@users.noreply.github.com>
2021-06-23 22:07:13 -0500

    550.1

--- .freeCodeCamp/test/540.test.js
+++ .freeCodeCamp/test/540.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getLastCommand, getFileContents } = require('./utils');
 
 describe('You', () => {
@@ -14,4 +14,4 @@ describe('You', () => {
 
     assert(correctCommand && /Number of times meow or meowzer appears:/.test(fileContents));
   });
-});
+});*/
---
+++ .freeCodeCamp/test/550.test.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+const { getLastCommand } = require('./utils');
+
+describe('You', () => {
+  it('should enter the suggested command', async () => {
+    const lastCommand = await getLastCommand();
+    const correctFlags = (lastCommand.indexOf('--color') > 0 || lastCommand.indexOf('--colour') > 0);
+    const filteredCommand = lastCommand.filter(part => part !== '--color' && part !== '--colour');
+
+    assert(correctFlags && filteredCommand[0] === 'grep' && (filteredCommand[1] === 'meow[a-z]*' || filteredCommand[1].pattern === 'meow[a-z]*') && filteredCommand[2] === 'kitty_ipsum_1.txt');
+  });
+});
