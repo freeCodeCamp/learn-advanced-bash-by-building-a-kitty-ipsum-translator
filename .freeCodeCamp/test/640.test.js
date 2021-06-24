fcd6320c0e77afb0020496f6a38b586c042e84e4
moT01 <20648924+moT01@users.noreply.github.com>
2021-06-24 09:20:33 -0500

    650.1

--- .freeCodeCamp/test/640.test.js
+++ .freeCodeCamp/test/640.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getLastCommand, getFileContents } = require('./utils');
 
 describe('tests', () => {
@@ -8,4 +8,4 @@ describe('tests', () => {
 
     assert(lastCommand[0] === 'sed' && lastCommand[1] === 's/r/2/' && lastCommand[2] === 'name.txt' && /freeCodeCamp/.test(fileContents));
   });
-});
+});*/
