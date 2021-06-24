482cb2ca841cae02f3a2de964c31d17ed4ce05c0
moT01 <20648924+moT01@users.noreply.github.com>
2021-06-24 09:21:06 -0500

    710.1

--- .freeCodeCamp/test/710.test.js
+++ .freeCodeCamp/test/710.test.js
@@ -1,4 +1,4 @@
-/*const assert = require('assert');
+const assert = require('assert');
 const { getLastCommand } = require('./utils');
 
 describe('You', () => {
@@ -7,6 +7,6 @@ describe('You', () => {
     const correctFlags = (lastCommand.indexOf('-n') > 0 && lastCommand.indexOf('-n') <= 3) || (lastCommand.indexOf('--line-number') > 0 && lastCommand.indexOf('--line-number') <= 3);
     const filteredCommand = lastCommand.filter(part => part !== '-n' && part !== '--line-number');
 
-    assert(correctFlags && filteredCommand[0] === 'grep' && (filteredCommand[1] === 'meow[a-z]*' || filteredCommand[1].pattern === 'meow[a-z]*') && filteredCommand[2] === 'kitty_ipsum_1.txt' && filteredCommand[3].op === '|' && filteredCommand[4] === 'sed' && filteredCommand[5] === 's/[0-9]/1/');
+    assert(correctFlags && filteredCommand[0] === 'grep' && (filteredCommand[1] === 'meow[a-z]*' || filteredCommand[1].pattern === 'meow[a-z]*') && filteredCommand[2] === 'kitty_ipsum_1.txt' && filteredCommand[3].op === '|' && filteredCommand[4] === 'sed' && filteredCommand[5] === 's/[0-9]+/1/');
   });
-});*/
+});
