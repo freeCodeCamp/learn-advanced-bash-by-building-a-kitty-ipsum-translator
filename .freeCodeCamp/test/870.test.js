3a74e5a137193b69082f3bba4d81f2c37daa5d0d
moT01 <20648924+moT01@users.noreply.github.com>
2021-06-27 19:22:19 -0500

    870.1

---
+++ .freeCodeCamp/test/870.test.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+const { getLastCommand, getFileContents } = require('./utils');
+
+describe('You', () => {
+  it('should enter the suggested command', async () => {
+    let lastCommand = await getLastCommand();
+    const fileContents = await getFileContents('../kitty_info.txt');
+
+    if (lastCommand[3] === '1') {
+      lastCommand.splice(3, 1);
+    }
+
+    const correctCommand = lastCommand[0] === 'echo' && lastCommand[1] === '-e' && lastCommand[2] === '\\nNumber of lines:' && lastCommand[3].op === '>>' && lastCommand[4] === 'kitty_info.txt';
+
+    assert(correctCommand && fileContents.match(/Number of lines:/g).length >= 2);
+  });
+});
