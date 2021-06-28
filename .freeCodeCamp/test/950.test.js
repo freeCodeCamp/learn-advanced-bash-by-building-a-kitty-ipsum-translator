098ec24e04073edcb4d97088d371e2a99705f337
moT01 <20648924+moT01@users.noreply.github.com>
2021-06-27 19:23:02 -0500

    960.1

--- .freeCodeCamp/test/950.test.js
+++ .freeCodeCamp/test/950.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getLastCommand, getFileContents } = require('./utils');
 
 describe('You', () => {
@@ -14,4 +14,4 @@ describe('You', () => {
 
     assert(correctCommand && fileContents.match(/Lines that they appear on:/).length >= 3);
   });
-});
+});*/
