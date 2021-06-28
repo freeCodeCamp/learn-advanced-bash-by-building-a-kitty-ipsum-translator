e9fc4e3ed24e296fa63d73a725b09f5c9e96f55c
moT01 <20648924+moT01@users.noreply.github.com>
2021-06-27 19:20:56 -0500

    770.1

--- .freeCodeCamp/test/760.test.js
+++ .freeCodeCamp/test/760.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getLastCommand } = require('./utils');
 
 describe('You', () => {
@@ -12,6 +12,6 @@ describe('You', () => {
       filteredCommand.splice(6, 1);
     }
 
-    assert(correctFlag1 && correctFlag2 && filteredCommand[0] === 'grep' && (filteredCommand[1] === 'meow[a-z]*' || filteredCommand[1].pattern === 'meow[a-z]*') && filteredCommand[2] === 'kitty_ipsum_1.txt' && filteredCommand[3].op === '|' && filteredCommand[4] === 'sed' && filteredCommand[5] === 's/([0-9]+).*/\\1/' && filteredCommand[6].op === '>>' && filteredCommand[7] === 'kitty_info.txt');
+    assert(correctFlag1 && correctFlag2 && filteredCommand[0] === 'grep' && (filteredCommand[1] === 'meow[a-z]*' || filteredCommand[1].pattern === 'meow[a-z]*') && filteredCommand[2] === 'kitty_ipsum_1.txt' && filteredCommand[3].op === '|' && filteredCommand[4] === 'sed' && filteredCommand[5] === 's/([0-9]+).*//*\\1/' && filteredCommand[6].op === '>>' && filteredCommand[7] === 'kitty_info.txt');
   });
-});
+});*/
