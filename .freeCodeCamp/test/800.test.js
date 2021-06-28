6e86a609cfcb572aff2f36638d95676c0a86b608
moT01 <20648924+moT01@users.noreply.github.com>
2021-06-27 19:21:37 -0500

    800.1

--- .freeCodeCamp/test/800.test.js
+++ .freeCodeCamp/test/800.test.js
@@ -1,4 +1,4 @@
-/*const assert = require('assert');
+const assert = require('assert');
 const { getLastCommand } = require('./utils');
 
 describe('You', () => {
@@ -7,6 +7,6 @@ describe('You', () => {
     const correctFlags = (lastCommand.indexOf('-o') > 0 || lastCommand.indexOf('--only-matching') > 0);
     const filteredCommand = lastCommand.filter(part => part !== '-o' && part !== '--only-matching');
 
-    assert(correctFlags && filteredCommand[0] === 'grep' && (filteredCommand[1] === 'cat[a-z]*' || filteredCommand[1].pattern === 'cat[a-z]*') && filteredCommand[2] === 'kitty_ipsum_1.txt');
+    assert(correctFlags && filteredCommand[0] === 'grep' && (filteredCommand[1] === 'cat[a-z]*' || filteredCommand[1].pattern === 'cat[a-z]*') && filteredCommand[2] === 'kitty_ipsum_1.txt' && filteredCommand[3].op === '|' && filteredCommand[4] === 'wc' && (filteredCommand[5] === '-l' || filteredCommand[5] === '--lines'));
   });
-});*/
+});
