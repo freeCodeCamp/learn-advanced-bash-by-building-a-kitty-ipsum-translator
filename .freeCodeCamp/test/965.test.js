80feecfa2151b11a9c477c51b6d51a5b9c8816c7
moT01 <20648924+moT01@users.noreply.github.com>
2021-06-27 19:23:07 -0500

    970.1

--- .freeCodeCamp/test/965.test.js
+++ .freeCodeCamp/test/965.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getLastCommand } = require('./utils');
 
 describe('You', () => {
@@ -9,4 +9,4 @@ describe('You', () => {
 
     assert(correctFlags && filteredCommand[0] === 'grep' && (filteredCommand[1] === 'cat[a-z]*' || filteredCommand[1].pattern === 'cat[a-z]*') && filteredCommand[2] === 'kitty_ipsum_2.txt');
   });
-});
+});*/
