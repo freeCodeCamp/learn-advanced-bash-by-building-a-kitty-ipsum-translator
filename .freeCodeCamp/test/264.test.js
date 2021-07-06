13709518eec65e94f97486855a28cddf6f9345f0
moT01 <20648924+moT01@users.noreply.github.com>
2021-07-06 13:29:04 -0500

    264.1

---
+++ .freeCodeCamp/test/264.test.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+const { getNextCommand } = require('./utils');
+
+describe('tests', () => {
+  it('should pass', async () => {
+    const nextCommand = await getNextCommand();
+
+    assert(nextCommand[0] === './script.sh' && (nextCommand[1] === undefined || nextCommand[1].op === ';'));
+  });
+});
