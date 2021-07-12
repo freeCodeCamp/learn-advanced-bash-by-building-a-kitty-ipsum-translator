1b44279ed8d5295fe1b0248bdff03876bdf39c2a
moT01 <20648924+moT01@users.noreply.github.com>
2021-07-12 18:47:44 -0500

    965.1

--- .freeCodeCamp/test/960.test.js
+++ .freeCodeCamp/test/960.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getLastCommand, getFileContents } = require('./utils');
 
 describe('You', () => {
@@ -10,4 +10,4 @@ describe('You', () => {
 
     assert(correctCommands && correctFlags && /\s+4\s+8\s+12\s+20\s+24\s+25\s*$/.test(fileContents));
   });
-});
+});*/
