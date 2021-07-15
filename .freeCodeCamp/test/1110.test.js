bf218e57182725bf417fde3657bd9b39b404f4cf
moT01 <20648924+moT01@users.noreply.github.com>
2021-07-14 21:53:23 -0500

    1110.1

--- .freeCodeCamp/test/1110.test.js
+++ .freeCodeCamp/test/1110.test.js
@@ -1,4 +1,4 @@
-/*const assert = require('assert');
+const assert = require('assert');
 const { getLastCommand } = require('./utils');
 
 describe('You', () => {
@@ -7,6 +7,6 @@ describe('You', () => {
     const correctFlag = (lastCommand.indexOf('--color') > 3 || lastCommand.indexOf('--colour') > 3);
     const filteredCommand = lastCommand.filter(part => part !== '--color' && part !== '--colour');
 
-    assert(correctFlag && filteredCommand[0] === './translate.sh' && (filteredCommand[1] === 'kitty_ipsum_1.txt' || filteredCommand[1] === './kitty_ipsum_1.txt') && filteredCommand[2].op === '|' && filteredCommand[3] === 'grep' && (filteredCommand[4] === 'dogchow' || filteredCommand[4].pattern === 'dogchow'));
+    assert(correctFlag && filteredCommand[0] === './translate.sh' && (filteredCommand[1] === 'kitty_ipsum_1.txt' || filteredCommand[1] === './kitty_ipsum_1.txt') && filteredCommand[2].op === '|' && filteredCommand[3] === 'grep' && (filteredCommand[4] === 'catnip' || filteredCommand[4].pattern === 'catnip'));
   });
-});*/
+});
