e0cd08d747ce4644d4187c78711e0aa3a3b16e06
moT01 <20648924+moT01@users.noreply.github.com>
2021-07-14 21:53:37 -0500

    1140.1

---
+++ .freeCodeCamp/test/1140.test.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+const { getFileContents } = require('./utils');
+
+describe('Your script file', () => {
+  it('should have the suggested command', async () => {
+    const scriptFile = await getFileContents('../translate.sh');
+
+    assert(/cat[ \t]+\$1[ \t]*\|[ \t]*sed[ \t]+('|")[ \t]*s\/catnip\/dogchow\/[ \t]*;[ \t]*s\/cat\/dog\/[ \t]*;[ \t]*s\/meow\/woof\/[ \t]*;?[ \t]*\1[ \t]*;?\s*$/g.test(scriptFile));
+  });
+});
